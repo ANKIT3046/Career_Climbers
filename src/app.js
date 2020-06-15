d576d505f8d63fecddf5d3a63c5b616c26c8c870
ANKIT3046 <40267545+ANKIT3046@users.noreply.github.com>
2020-06-15 23:00:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,17 @@ const Courses = require('./models/courses');
 const path = require('path')
 const hbs = require('hbs')
 const course_list = require('./utils/course_list');
+//const passing = require('./utils/test.js');
 
 //const getData = require('./utils/test.js')
+//------------
+const request = require('request');
+const cheerio = require('cheerio');
+
+// Install node-fetch before running the code using the command- "npm install --save node-fetch"
+const fetch = require('node-fetch')
+//-------------
+
 
 
 
@@ -20,19 +29,69 @@ app.set('views',viewsPath);
 hbs.registerPartials(partialsPath);
  // Static Directory to serve
 
- app.use(express.static(publicDirectoryPath));
+app.use(express.static(publicDirectoryPath));
 
 app.use(express.json())
+
+
+var url_list=["https://www.udemy.com/course/react-the-complete-guide-incl-redux/",
+              "https://www.udemy.com/course/mongodb-the-complete-developers-guide/",
+              "https://www.udemy.com/course/ios-13-app-development-bootcamp/",
+              "https://www.udemy.com/course/the-complete-react-native-and-redux-course/",
+              "https://www.udemy.com/course/the-web-developer-bootcamp/",
+              "https://www.udemy.com/course/creativity-and-innovation-for-business/"]
+
 
 
 
 
 app.get('',(req,res,next) =>{
-    res.render('index',course_list)
+  async function my_detail(html,url){
+      try{
+          const $ = await cheerio.load(html);
+          const siteHeading = $('.row');
+
+          const output = siteHeading.find('h1').text().replace("\n",'');
+
+          const image = $('.introduction-asset img').attr('src')
+          // console.log(image)
+          // console.log(output)
+
+
+          const details={
+          img:image,
+          name:output,
+          link:url
+          }
+          return await details
+      }catch(e){
+          console.log("ERROR: Could not parse")
+          return null
+      }
+  }
+getDataFromURL(url_list)
+async function getDataFromURL(url_list){
+    var new_l=[]
+    // for every link. fetch the data, get the html response and pass it to my_detail function to get name and img
+    url_list.forEach(url=>{
+        fetch(url)
+        .then(res=> res.text())
+        .then(html=>my_detail(html,url))
+        .then(detail=>new_l.push(detail))
+
+    })
+
+    setTimeout(function(){
+        p={courses:new_l}
+        res.render('index',p)
+        console.log(p)
+    },4000)
+}
+
+    //res.render('index',course_list)
+
 })
 
-
-// this is for posting course of your choice to the database by admin
 app.post('/courses', async (req,res) =>{
   // console.log(req.body)
   // res.send('testing')
@@ -46,8 +105,8 @@ app.post('/courses', async (req,res) =>{
   }
 })
 
-// this is for rendring the course details on page here i use common link which use previeously to store in database
-app.get('/courses', async (req,res,next) =>{
+
+app.get('/additional_course', async (req,res,next) =>{
 
   try {
     const course =await Courses.find({})
@@ -62,7 +121,7 @@ app.get('/courses', async (req,res,next) =>{
   }
 })
 
-// this for deleting perticuler course from databse by admin 
+
 
 app.delete('/courses/:id', async (req,res) =>{
   try{
